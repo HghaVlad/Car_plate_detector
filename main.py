f7298b5e32d6f05c67fbb63bf8b8b8a65cc5989c
Vlad Bukharin <vladbax6@gmail.com>
2023-10-11 21:10:39 +0300

    Workable code

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import numpy as np
 from ultralytics import YOLO
 from paddleocr import PaddleOCR
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from torch import Tensor
 
 
 YOLO_MODEL_PT = "yolosmall7.pt"
+PADDLE_MODEL_PATH = "ocr_model/"
 
-ORGINAL_IMG_PATH = "mile.png"
-RESULT_PATH = r"E:\\Code works\Data Science\\MIEM_carplate_detector\\Car_plate_detector\\images"
+ORGINAL_IMG_PATH = "test.jpg"
+RESULT_PATH = "images"
 
+
+TEXT_FONT = ImageFont.truetype("arial.ttf", 40)
+TEXT_COLOUR = "green"
 
 def init_yolo() -> YOLO:
     model = YOLO(YOLO_MODEL_PT)
-    print("Model is ready")
+    print("YOLO Model is ready")
     return model
 
 
@@ -39,22 +43,27 @@ def draw_box(boxes: list, lables: list, image: Image) -> Image:
     draw = ImageDraw.Draw(image)
     for box, label in zip(boxes, lables):
         x1, y1, x2, y2 = map(lambda x: round(x.item()), box[:4])
-        draw.rectangle((x1, y1, x2, y2))
-        draw.text((x1, y1-20), str(label), fill="red")
+        draw.rectangle((x1, y1, x2, y2), width=5)
+        draw.text((x1, y1-40), label, fill=TEXT_COLOUR, font=TEXT_FONT)
     return image
 
 
 def init_paddleocr():
-    ocr = PaddleOCR(use_angle_cls=False, lang="en")
+    ocr = PaddleOCR(rec_model_dir=PADDLE_MODEL_PATH, use_angle_cls=False, lang="en", show_log=False)
+    print("Paddle Model is Ready")
     return ocr
 
 def recognise_text(image, model):
     result = model.ocr(np.asarray(image) , cls=True)
-    print(result)
     if result[0] is None:
         return "NOT DETECTED"
     
-    return result[0][0][1][0]
+    
+    return clear_format("".join([x[1][0] for x in result[0]]))
+
+
+def clear_format(recognised_string: str) -> str:
+    return recognised_string.replace("RUS", "")
 
 
 
@@ -68,9 +77,9 @@ if __name__ == "__main__":
     plates = crop_image(current_image, boxes)
     labels = []
     for i, plate in enumerate(plates):
-        plate[0].save(f"{RESULT_PATH}\\frame{i}.png")
         label = recognise_text(plate[0], ocr_model)
         labels.append(label)
-    
+        plate[0].save(f"{RESULT_PATH}\\frame{label}.png")
+
     output_image = draw_box(boxes, labels, current_image)
     output_image.save(RESULT_PATH + "\\frame_end.png")
